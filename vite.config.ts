090256850b43c0be8d2407f58cdeb8f85ca05559
theworldhappiest <ccucontrollab203@gmail.com>
2026-01-29 20:20:21 +0800

    設定 GitHub Pages 部署環境

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
 
     return {
-      base: './', 
+      base: '/CCULab203Website/',
 
       server: {
         port: 3000,
